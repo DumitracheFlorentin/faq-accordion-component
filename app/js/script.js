d700da7983ecca85e9784e165e46e556582ac2f0
Florentin Dumitrache <dumitracheflorentincristian@gmail.com>
2021-09-06 19:44:46 +0300

    Done the JS part

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,31 @@
-console.log("It works!");
+const cardUI = document.querySelectorAll(".card");
+
+cardUI.forEach((card) => {
+  card.addEventListener("click", (e) => {
+    if (
+      !e.target.parentElement.parentElement.children[1].classList.contains(
+        "check"
+      )
+    ) {
+      e.target.parentElement.children[0].classList.add("titleCheck");
+      e.target.parentElement.parentElement.children[1].classList.add("check");
+    } else if (
+      e.target.parentElement.parentElement.children[1].classList.contains(
+        "check"
+      )
+    ) {
+      e.target.parentElement.children[0].classList.remove("titleCheck");
+      e.target.parentElement.parentElement.children[1].classList.remove(
+        "check"
+      );
+    }
+
+    if (!e.target.parentElement.children[1].classList.contains("check")) {
+      e.target.children[0].classList.add("titleCheck");
+      e.target.parentElement.children[1].classList.add("check");
+    } else if (e.target.parentElement.children[1].classList.contains("check")) {
+      e.target.children[0].classList.remove("titleCheck");
+      e.target.parentElement.children[1].classList.remove("check");
+    }
+  });
+});
